29818762e68cf1dd99c84d380cbd24cef1ef1e88
dgongut <dgonzalezgutierrez1@gmail.com>
2026-09-16 09:47:17 +0200

    Versión 5.0.0_RC3a en Dockerfile y core

--- core.py
+++ core.py
@@ -35,7 +35,7 @@ from i18n import get_text, language
 from logger import debug, error, warning
 from message_queue import MessageQueue
 
-VERSION = "5.0.0_RC3"
+VERSION = "5.0.0_RC3a"
 
 _unmute_timer = None
 _mute_lock = threading.Lock()  # Lock for thread-safe mute timer operations
